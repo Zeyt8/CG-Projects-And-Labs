24e1f7d212fde18d25439685a12043294feeab78
Foca Bogdan <81586357+Zeyt8@users.noreply.github.com>
2023-11-16 20:46:04 +0200

    Start Tema1

---
+++ src/lab_m2/Tema1/shaders/CubeMap.FS.glsl
@@ -0,0 +1,40 @@
+#version 430
+
+// Input
+layout(location = 0) in vec3 world_position;
+layout(location = 1) in vec3 world_normal;
+
+// Uniform properties
+uniform samplerCube texture_cubemap;
+uniform int type;
+
+uniform vec3 camera_position;
+
+// Output
+layout(location = 0) out vec4 out_color;
+
+
+vec3 myReflect()
+{
+    // Compute the reflection color value
+    return texture(texture_cubemap, reflect(normalize(world_position - camera_position), world_normal)).rgb;
+}
+
+
+vec3 myRefract(float refractive_index)
+{
+    // Compute the refraction color value
+    return texture(texture_cubemap, refract(normalize(world_position - camera_position), world_normal, 1.0 / refractive_index)).rgb;
+}
+
+
+void main()
+{
+    if (type == 0)
+    {
+        out_color = vec4(myReflect(), 0);
+    }
+    else
+    {
+    }
+}
