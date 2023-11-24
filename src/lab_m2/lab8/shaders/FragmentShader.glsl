6a4c42d7b0226378f3a205d3f22afa7a7828b9a2
Foca Bogdan <81586357+Zeyt8@users.noreply.github.com>
2023-11-24 09:33:15 +0200

    Lab 8

--- src/lab_m2/lab8/shaders/FragmentShader.glsl
+++ src/lab_m2/lab8/shaders/FragmentShader.glsl
@@ -14,7 +14,8 @@ layout(location = 0) out vec4 out_color;
 
 // Local variables
 vec2 textureCoord = vec2(texture_coord.x, (flipVertical != 0) ? 1 - texture_coord.y : texture_coord.y); // Flip texture
-
+float grayscales[100];
+vec4 colors[100];
 
 vec4 grayscale()
 {
@@ -40,6 +41,40 @@ vec4 blur(int blurRadius)
     return sum / samples;
 }
 
+vec4 median(int blurRadius)
+{
+    vec2 texelSize = 1.0f / screenSize;
+    for (int i = -blurRadius; i <= blurRadius; i++)
+	{
+		for(int j = -blurRadius; j <= blurRadius; j++)
+		{
+            vec4 color = texture(textureImage, textureCoord + vec2(i, j) * texelSize);
+            colors[i * blurRadius + j] = color;
+			grayscales[i * blurRadius + j] = 0.21 * color.r + 0.71 * color.g + 0.07 * color.b;
+		}
+	}
+    for (int i = 0; i < blurRadius * blurRadius; i++)
+	{
+		int min = i;
+		for (int j = i + 1; j < blurRadius * blurRadius; j++)
+		{
+			if (grayscales[j] < grayscales[min])
+			{
+				min = j;
+			}
+		}
+        // swap grayscales
+		float temp = grayscales[i];
+		grayscales[i] = grayscales[min];
+		grayscales[min] = temp;
+        // swap colors
+        vec4 tempColor = colors[i];
+        colors[i] = colors[min];
+        colors[min] = tempColor;
+	}
+    return colors[blurRadius * blurRadius / 2];
+}
+
 
 void main()
 {
@@ -56,6 +91,12 @@ void main()
             out_color = blur(3);
             break;
         }
+
+        case 3:
+        {
+			out_color = median(7);
+			break;
+		}
 
         default:
             out_color = texture(textureImage, textureCoord);
